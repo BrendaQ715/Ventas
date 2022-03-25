0197092664b140b8f18f9413c30eb770c3cdb1e9
qbren <qbren@LAPTOP-3OF65QBH>
2022-03-24 21:55:09 -0700

    Agregar archivos de proyecto.

---
+++ Ventas/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Ventas
+{
+    internal class Program
+    {
+        static string NombreProducto;
+        
+        static int Nocliente;
+        static int cantidad;
+        static double precio;
+        static double total;
+        
+        
+        
+        
+        static string[] campos = new string[5];
+        
+        static void Main(string[] args)
+
+        {    //AgregarTexto();
+            //LeerDocumento();
+            Menu();
+            CrearCompra();
+         
+           
+
+
+        }
+        static void Menu()
+        { byte opcion;
+            opcion = 0;
+
+                do
+                {
+                    Console.WriteLine("Menu de opciones ");
+                    Console.WriteLine("1. Registro de ventas ");
+                    Console.WriteLine("2.Salir");
+                    Console.WriteLine("Que  opcion deseas realizar...");
+                    opcion=Convert.ToByte(Console.ReadLine());
+                    switch (opcion)
+                    {
+                    case 1:
+                        CrearCompra();
+
+                            break;
+                        
+                        case 2:
+                            Console.WriteLine("-------Operacion finalizada-------");
+                            break;
+                            default:
+                            Console.WriteLine("------La opcion no es correcta -------");
+                            break;
+                    }
+
+
+            }
+            while (opcion != 5);
+           
+
+        
+        
+        }
+        
+        public static void CrearCompra()
+        {
+           
+            StreamWriter venta;
+
+
+            string ventas = "C:\\Users\\qbren\\Documents\\Ventas\\registroventas.txt";
+
+            venta = File.AppendText(ventas);
+            venta.WriteLine();
+
+            
+          
+                Console.WriteLine("Ingresa n° de cliente");
+                Nocliente = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Nombre del producto");
+                NombreProducto = Console.ReadLine();
+
+                Console.WriteLine("Cantidad de productos");
+                cantidad = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("precio de los productos");
+                precio=Convert.ToDouble(Console.ReadLine()); 
+
+            total = precio * cantidad;
+                Console.WriteLine("El total de la compra es de:"+total);
+                
+                
+                 venta.WriteLine(Nocliente + "," + NombreProducto + "," + cantidad + "," + precio + "," + total + ",");
+                Console.WriteLine("Registro de venta relizado...");
+
+
+            venta.Close();
+        }
+
+    
+            
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+
---
+++ Ventas/Venta.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ventas
+{
+    internal class Venta
+    {
+        public int cantidad { get; set; }
+        public string Nombreventa { get; set; }
+        public int Precio { get; set; }
+
+        public Venta(int cantidad, string Nombreventa, int precio)
+        {
+            this.cantidad = cantidad;
+            this.Nombreventa = Nombreventa;
+            this.Precio = precio;
+
+        }
+        public void Pago(double precio, int cantidad)
+        {
+            double total = (cantidad * precio);
+        }
+
+    }
+
+}
